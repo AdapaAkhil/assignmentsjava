c0acf634d6870cb523fd673ad4247ca5e136dd3b
AdapaAkhil <101463608+AdapaAkhil@users.noreply.github.com>
2022-03-19 22:55:15 +0530

    Update ProductMain.java

--- Assignment4/src/assign4/ProductMain.java
+++ Assignment4/src/assign4/ProductMain.java
@@ -1,5 +1,16 @@
 package assign4;
 
 public class ProductMain {
+  public static void main(String[] args) {
+		Queue<Product> Productqueue = new PriorityQueue<>();
+		Productqueue.add(new Product(12,"Flour",45));
+        Productqueue.add(new Product(123,"Rice",67));
+        Productqueue.add(new Product(1236,"Toothpaste",20));
+        Productqueue.add(new Product(1232,"Almonds",500));
+        Productqueue.add(new Product(1234,"Tide",400));
+        while (!Productqueue.isEmpty()) {
+            System.out.println(Productqueue.remove());
+        }
+	  }
 
 }
